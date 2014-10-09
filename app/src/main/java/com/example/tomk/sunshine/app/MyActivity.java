cf1b162c366add0a729e86178fd078820b762c9b
Tom Kiefhaber <tkiefhab@gmail.com>
2014-10-08 22:32:14 -0400

    move request to asynctask, own file, add refresh menue item, does not refresh yet

--- app/src/main/java/com/example/tomk/sunshine/app/MyActivity.java
+++ app/src/main/java/com/example/tomk/sunshine/app/MyActivity.java
@@ -1,21 +1,9 @@
 package com.example.tomk.sunshine.app;
 
-import android.support.v7.app.ActionBarActivity;
-import android.support.v7.app.ActionBar;
-import android.support.v4.app.Fragment;
 import android.os.Bundle;
-import android.view.LayoutInflater;
+import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
-import android.view.ViewGroup;
-import android.os.Build;
-import android.widget.ArrayAdapter;
-import android.widget.ListView;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 
 
 public class MyActivity extends ActionBarActivity {
@@ -26,7 +14,7 @@ public class MyActivity extends ActionBarActivity {
         setContentView(R.layout.activity_my);
         if (savedInstanceState == null) {
             getSupportFragmentManager().beginTransaction()
-                    .add(R.id.container, new PlaceholderFragment())
+                    .add(R.id.container, new ForecastFragment())
                     .commit();
         }
     }
@@ -51,43 +39,4 @@ public class MyActivity extends ActionBarActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
-
-            String[] data = {
-                    "Today - Sunny - 88/63",
-                    "Tomorrow - Cloudy - 77/50",
-                    "Thursday - Foggy - 65/60",
-                    "Friday - Cloudy - 67/57",
-                    "Saturday - Armageddon - 80/70",
-                    "Sunday - Heavy Rain - 75/70",
-                    "Monday - Sunny - 80/79"
-            };
-
-            List<String> weekForecast = new ArrayList<String>(Arrays.asList(data));
-
-            ArrayAdapter<String> forecastAdapter =
-                    new ArrayAdapter<String>(
-                            getActivity(),
-                            R.layout.list_item_forecast,
-                            R.id.list_item_forecast_textview,
-                            weekForecast
-                    );
-            View rootView = inflater.inflate(R.layout.fragment_my, container, false);
-
-            ListView listView = (ListView) rootView.findViewById(R.id.listview_forecast);
-            listView.setAdapter(forecastAdapter);
-
-            return rootView;
-        };
-    }
 }
