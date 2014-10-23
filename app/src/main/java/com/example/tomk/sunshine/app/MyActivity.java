4fc1c813e366b0ed7d2867071f2729a7a683ddba
Tom Kiefhaber <tkiefhab@gmail.com>
2014-10-22 22:52:24 -0400

    add map intent

--- app/src/main/java/com/example/tomk/sunshine/app/MyActivity.java
+++ app/src/main/java/com/example/tomk/sunshine/app/MyActivity.java
@@ -1,15 +1,22 @@
 package com.example.tomk.sunshine.app;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
+import android.net.Uri;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
 import android.support.v7.app.ActionBarActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import static android.net.Uri.*;
 
 
 public class MyActivity extends ActionBarActivity {
 
-    @Override
+    private final String LOG_TAG = MyActivity.class.getSimpleName();
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my);
@@ -17,6 +24,22 @@ public class MyActivity extends ActionBarActivity {
             getSupportFragmentManager().beginTransaction()
                     .add(R.id.container, new ForecastFragment())
                     .commit();
+        }
+    }
+
+    private void openPreferredLocationInMap() {
+        SharedPreferences sharedPrefs = PreferenceManager.getDefaultSharedPreferences(this);
+        String location = sharedPrefs.getString(
+          getString(R.string.pref_location_key),
+          getString(R.string.pref_location_default)
+        );
+        Uri geoLocation = parse("geo:0;0?").buildUpon().appendQueryParameter("q", location).build();
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.setData(geoLocation);
+        if (intent.resolveActivity(getPackageManager()) != null) {
+            startActivity(intent);
+        } else {
+            Log.d(LOG_TAG, "couldn't call " + location + ", no receiving apps installed");
         }
     }
 
@@ -36,6 +59,10 @@ public class MyActivity extends ActionBarActivity {
         int id = item.getItemId();
         if (id == R.id.action_settings) {
             startActivity(new Intent(this, com.example.tomk.sunshine.app.SettingsActivity.class));
+            return true;
+        }
+        if (id == R.id.action_map) {
+            openPreferredLocationInMap();
             return true;
         }
         return super.onOptionsItemSelected(item);
